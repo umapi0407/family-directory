ee4e4f26e47d46d42c9fa04cdb0924a18a337009
Umang Metaliya <167159513+umapi0407@users.noreply.github.com>
2026-01-17 18:05:52 +0530

    UI updates

--- client/src/components/FamilyMemberCard.jsx
+++ client/src/components/FamilyMemberCard.jsx
@@ -1,11 +1,21 @@
 export default function FamilyMemberCard({ member }) {
-  const formatDate = (date) => {
-    return new Date(date).toLocaleDateString("en-US", {
-      year: "numeric",
-      month: "long",
-      day: "numeric",
-    })
-  }
+  const formatDateMasked = (data) => {
+    const d = new Date(date);
+
+    const day = d.getDate();
+    const month = d.toLocaleString("en-US", {month: "long"});
+    const year = d.getFullYear().toString();
+
+    const maskedYear = year.slice(0,,2) + "**";
+
+    return `${day} ${month} ${maskedYear}`;
+  // const formatDate = (date) => {
+  //   return new Date(date).toLocaleDateString("en-US", {
+  //     year: "numeric",
+  //     month: "long",
+  //     day: "numeric",
+  //   })
+  // }
 
   return (
     <div className="card">
@@ -19,7 +29,7 @@ export default function FamilyMemberCard({ member }) {
       <div className="card-content">
         <h3>{member.fullName}</h3>
         <p className="surname">Surname: {member.surname}</p>
-        <p className="dob">DOB: {formatDate(member.dateOfBirth)}</p>
+        <p className="dob">DOB: {formatDateMasked(member.dateOfBirth)}</p>
       </div>
     </div>
   )
